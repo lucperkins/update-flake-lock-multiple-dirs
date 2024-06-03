dc15c350a530c956e21a29952dda03a9446eb412
Luc Perkins <lucperkins@gmail.com>
2024-06-03 14:02:22 -0700

    Add nix input

--- flake.nix
+++ flake.nix
@@ -3,8 +3,9 @@
   description = "update-flake-lock test sub-flake";
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nix.url = "https://flakehub.com/f/DeterminateSystems/nix/*.tar.gz";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nix }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-darwin" "aarch64-linux" "x86_64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
